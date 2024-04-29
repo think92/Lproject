b9f5a8aaaaf9f798a66bf07a3a8cf39fcb98c339
이승재 <158141455+think92@users.noreply.github.com>
2024-04-29 11:11:43 +0900

    g
    g

--- project/src/Editor.jsx
+++ project/src/Editor.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./css/Editor.css";
 import MainBar from "./MainBar";
@@ -11,12 +11,39 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 
 const Editor = () => {
+  const fileInputRef = useRef(null);
+  const [imageView, setImageView] = useState(null);
   useEffect(() => {
     document.body.style.backgroundColor = "black";
   }, []);
+
+  const handleButtonClick = () => {
+    fileInputRef.current.click();
+  };
+
+  const handleImageChange = (e) => {
+    e.preventDefault();
+
+    let reader = new FileReader();
+    let file = e.target.files[0];
+
+    reader.onloadend = () => {
+      setImageView(reader.result);
+    };
+    if (file) {
+      reader.readAsDataURL(file);
+    }
+  };
+
   return (
     <div className="editor-specific">
       <MainBar />
+      <input
+        type="file"
+        style={{ display: "none" }}
+        ref={fileInputRef}
+        onChange={handleImageChange}
+      />
       <section className="sec">
         <div className="buttons">
           <p className="auto">Auto Mosaic</p>
@@ -75,15 +102,15 @@ const Editor = () => {
             </div>
           </div>
           <div className="submits">
-            <button className="submit active">사진/영상 업로드</button>
+            <button onClick={handleButtonClick} className="submit active">
+              사진/영상 업로드
+            </button>
             <button className="submit">저장</button>
             <button className="submit">삭제</button>
           </div>
         </div>
         <div className="edit">
-          <p>이미지</p>
-          <p>이미지</p>
-          <p>이미지</p>
+          {imageView ? <img src={imageView} alt="이미지" /> : <p>오류</p>}
         </div>
       </section>
     </div>
