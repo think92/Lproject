1202db2af58039c8c25d326ef8c97476784ea077
김도원 <158456293+Kim-doni@users.noreply.github.com>
2024-05-13 10:37:41 +0900

    240513_로그인 이메일 수정
    240513_로그인 이메일 수정

--- project/src/Login.jsx
+++ project/src/Login.jsx
@@ -27,14 +27,14 @@ const Login = () => {
       )
       .then((res) => {
         console.log("로그인 정보보기  : ", res.data);
-        console.log("로그인 이메일    : ", res.data.mbEmail);
+        console.log("로그인 이메일    : ", res.data.mb_email);
         console.log("로그인 비번      : ", res.data.mb_pw);
         console.log("로그인 등급      : ", res.data.mb_role);
         console.log("로그인 가입일자  : ", res.data.joinedAt);
 
-        if (res.data.mbEmail !== undefined) {
+        if (res.data.mb_email !== undefined) {
           // 회원 정보 context에 담기
-          setLogin_id(res.data.mbEmail);
+          setLogin_id(res.data.mb_email);
           setLogin_role(res.data.mb_role);
 
           nav("/");
