f02c1ac768269d65253eaa0fb272825a77f7028a
전송민 <70497093+jeonsongmin@users.noreply.github.com>
2024-05-17 15:03:07 +0900

    JSM
    [관리자] 문의사항 답변 INSERT, 문의사항 답변여부 UPDATE, 문의 답변 LIST형식 전달 완료 . 모두

--- project/src/component/Modal.jsx
+++ project/src/component/Modal.jsx
@@ -1,13 +1,56 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import "../css/modal.css";
+import axios from "axios";
 
 const Modal = ({ isOpen, onClose, inquiry }) => {
+  const qstnsToAnswer = useRef(); // 답변 내용
+
   if (!isOpen || !inquiry) return null;
 
   const formatDate = (dateString) => {
     const date = new Date(dateString);
     return date.toLocaleString();
   };
+
+  // 문의사항의 답변 보기
+  const formData = new FormData();
+  formData.append("qstn_idx", inquiry.qstn_idx); // 문의사항의 답변 내용
+  console.log("문의 고유번호  : ", formData.get("qstn_idx"));
+
+  axios
+    .post(`http://localhost:8083/AdmApi/adminAnswer`, formData, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+    .then((res) => {
+      console.log("문의 답변보기  : ", res.data);
+    });
+
+  // 답변 저장
+
+  function answer() {
+    const ansFormData = new FormData();
+
+    ansFormData.append("qstn_idx", inquiry.qstn_idx); // 문의사항의 고유 번호
+    ansFormData.append("ans_content", qstnsToAnswer.current.value); // 문의사항의 답변 내용
+    ansFormData.append("admin_id", "admin"); // 문의 답변한 관리자(추후 변경하도록)
+
+    console.log("[보냄] 문의 고유번호  : ", ansFormData.get("qstn_idx"));
+    console.log("[보냄] 문의 답변내용  : ", ansFormData.get("ans_content"));
+    console.log("[보냄] 문의 답변관리자  : ", ansFormData.get("admin_id"));
+
+    axios
+      .post(`http://localhost:8083/AdmApi/adminToAnswer`, ansFormData, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+      .then((res) => {
+        console.log("문의 답변등록 여부  : ", res.data);
+        alert("답변이 저장되었습니다.");
+      });
+  }
 
   return (
     <div className="modal">
@@ -33,6 +76,7 @@ const Modal = ({ isOpen, onClose, inquiry }) => {
         </div>
         <div className="modal-body">
           <textarea
+            ref={qstnsToAnswer}
             className="styled-textarea"
             placeholder="답변을 작성하세요"
           ></textarea>
@@ -41,10 +85,7 @@ const Modal = ({ isOpen, onClose, inquiry }) => {
           <button className="button" onClick={onClose}>
             닫기
           </button>
-          <button
-            className="button"
-            onClick={() => alert("답변이 저장되었습니다.")}
-          >
+          <button className="button" onClick={answer}>
             저장
           </button>
         </div>
