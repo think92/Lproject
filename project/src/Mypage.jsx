cc1c21b929c1c8e4ad35adbfb6297feb773eec5c
전송민 <70497093+jeonsongmin@users.noreply.github.com>
2024-05-25 20:14:55 +0900

    jsm
    마이페이지 >작업내역 > 체크한 리스트 url 담기

--- project/src/Mypage.jsx
+++ project/src/Mypage.jsx
@@ -5,9 +5,10 @@ import axios from "axios";
 import { Link } from "react-router-dom";
 
 const Mypage = () => {
-  const [today, setToday] = useState(new Date()); // 오늘 날자
+  // const [today, setToday] = useState(new Date()); // 오늘 날자
   const [memberImage, setMemberImage] = useState([]); // 회원의 모든 작업내역
   const [selectedDate, setSelectedDate] = useState(); // 선택한 날짜 (예시)
+  const [medias, setMedias] = useState([]); // 세션 미디어 관련
 
   const formData = new FormData();
   formData.append("mb_email", sessionStorage.getItem("mb_email"));
@@ -22,17 +23,22 @@ const Mypage = () => {
       })
       .then((res) => {
         setMemberImage(res.data); // 회원 작업내역의 이미지 정보 배열형식
+        // 날자 정렬
         setSelectedDate(
           res.data.sort((a, b) => new Date(b.date) - new Date(a.date))
         );
-        console.log("날자추출 : ", res.data[0].createdAt.split("T")[0]);
-        console.log("정렬 : ", selectedDate);
+        console.log(
+          "정렬 : ",
+          res.data.sort((a, b) => new Date(b.date) - new Date(a.date))
+        );
+        console.log("■■■■■ medias : ", sessionStorage.getItem("medias"));
       })
       .catch((err) => {
         console.error("Error data:", err);
       });
   }, []);
 
+  // 날자별 미디어 분류
   const imagesByDate = memberImage.reduce((acc, memberImage) => {
     if (!acc[memberImage.createdAt.split("T")[0]]) {
       acc[memberImage.createdAt.split("T")[0]] = [];
@@ -40,6 +46,33 @@ const Mypage = () => {
     acc[memberImage.createdAt.split("T")[0]].push(memberImage);
     return acc;
   }, {});
+
+  // 체크박스 클릭시 미디어 정보 넣기 2
+  const [checkedList, setCheckedList] = useState([]);
+
+  const checkedItemHandler = (value, isChecked) => {
+    console.log("작동2");
+    if (isChecked) {
+      setCheckedList((prev) => [...prev, value]);
+    } else {
+      setCheckedList(checkedList.filter((item) => item !== value));
+    }
+  };
+
+  // 체크박스 클릭시 미디어 정보 넣기 1
+  const handleCheckboxChange = (e, value) => {
+    console.log("작동1");
+    const isChecked = e.target.checked;
+    checkedItemHandler(value, isChecked);
+  };
+
+  // 수정시 세션에 회원이메일 기준으로 미디어 정보 넣기
+  const mediasUpdate = () => {
+    const mb_email = sessionStorage.getItem("mb_email");
+    if (mb_email) {
+      sessionStorage.setItem("medias", JSON.stringify(checkedList));
+    }
+  };
 
   return (
     <div>
@@ -76,7 +109,9 @@ const Mypage = () => {
                   <div className="MosaicDate">
                     <div className="MosaicDates">{date}</div>
                     <div>
-                      <button className="MosaicFix">수정</button>
+                      <button className="MosaicFix" onClick={mediasUpdate}>
+                        수정
+                      </button>
                     </div>
                   </div>
 
@@ -87,13 +122,19 @@ const Mypage = () => {
                         <div className="ImgCheckBox">
                           <img
                             className="MosaicImgs"
-                            src={
-                              "https://s3testsm.s3.amazonaws.com/" +
-                              image.file_name
-                            }
+                            src={`https://${process.env.REACT_APP_AWS_BUCKET}.s3.${process.env.REACT_APP_REGION}.amazonaws.com/${image.file_name}`}
                             alt=""
                           />
-                          <input type="checkbox" id="ImgCheck" />
+                          <input
+                            type="checkbox"
+                            id="ImgCheck"
+                            onChange={(e) =>
+                              handleCheckboxChange(
+                                e,
+                                `https://${process.env.REACT_APP_AWS_BUCKET}.s3.${process.env.REACT_APP_REGION}.amazonaws.com/${image.file_name}`
+                              )
+                            }
+                          />
                         </div>
                       </div>
                     ))}
