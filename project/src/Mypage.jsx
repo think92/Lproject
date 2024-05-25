72fdc98068c258f2230bb9a36869798256578502
전송민 <70497093+jeonsongmin@users.noreply.github.com>
2024-05-25 17:56:14 +0900

    jsm
    마이페이지 작업이력 날자별 리스트로 구현

--- project/src/Mypage.jsx
+++ project/src/Mypage.jsx
@@ -1,14 +1,19 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "./css/Mypage.css";
 import MypageBar from "./MypageBar";
 import axios from "axios";
 import { Link } from "react-router-dom";
 
 const Mypage = () => {
+  const [today, setToday] = useState(new Date()); // 오늘 날자
+  const [memberImage, setMemberImage] = useState([]); // 회원의 모든 작업내역
+  const [selectedDate, setSelectedDate] = useState(); // 선택한 날짜 (예시)
+
   const formData = new FormData();
   formData.append("mb_email", sessionStorage.getItem("mb_email"));
 
   useEffect(() => {
+    // 회원의 모든 작업내역 불러오기
     axios
       .post("http://localhost:8083/MemApi/Mypage", formData, {
         headers: {
@@ -16,12 +21,25 @@ const Mypage = () => {
         },
       })
       .then((res) => {
-        console.log(res.data);
+        setMemberImage(res.data); // 회원 작업내역의 이미지 정보 배열형식
+        setSelectedDate(
+          res.data.sort((a, b) => new Date(b.date) - new Date(a.date))
+        );
+        console.log("날자추출 : ", res.data[0].createdAt.split("T")[0]);
+        console.log("정렬 : ", selectedDate);
       })
       .catch((err) => {
         console.error("Error data:", err);
       });
   }, []);
+
+  const imagesByDate = memberImage.reduce((acc, memberImage) => {
+    if (!acc[memberImage.createdAt.split("T")[0]]) {
+      acc[memberImage.createdAt.split("T")[0]] = [];
+    }
+    acc[memberImage.createdAt.split("T")[0]].push(memberImage);
+    return acc;
+  }, {});
 
   return (
     <div>
@@ -53,95 +71,90 @@ const Mypage = () => {
             <input type="date" id="date" className="tooldate"></input>
             {/* 스크롤바 전체 창 */}
             <div className="ToolTotal">
-              <div className="MosaicDate">
-                <div className="MosaicDates">2024-05-08</div>
+              {Object.keys(imagesByDate).map((date) => (
                 <div>
-                  <button className="MosaicFix">수정</button>
-                </div>
-              </div>
-              <div className="MosaicImg">
-                <div className="MosaicImgs">
-                  <div className="ImgCheck"></div>
-                </div>
-                <div className="MosaicImgs">
-                  <div className="ImgCheck"></div>
-                </div>
-                <div className="MosaicImgs">
-                  <div className="ImgCheck"></div>
-                </div>
-                <div className="MosaicImgs">
-                  <div className="ImgCheck"></div>
-                </div>
-                <div className="MosaicImgs">
-                  <div className="ImgCheck"></div>
-                </div>
-                <div className="MosaicImgs">
-                  <div className="ImgCheck"></div>
-                </div>
-                <div className="MosaicImgs">
-                  <div className="ImgCheck"></div>
-                </div>
-                <div className="MosaicImgs">
-                  <div className="ImgCheck"></div>
-                </div>
-                <div className="MosaicImgs">
-                  <div className="ImgCheck"></div>
-                </div>
-              </div>
+                  <div className="MosaicDate">
+                    <div className="MosaicDates">{date}</div>
+                    <div>
+                      <button className="MosaicFix">수정</button>
+                    </div>
+                  </div>
 
-              <div className="MosaicDate2">
-                <div className="MosaicDates">2024-05-07</div>
-                <div>
-                  <button className="MosaicFix">수정</button>
+                  {/* ////////////////////////////// */}
+                  <div className="MosaicImg">
+                    {imagesByDate[date].map((image) => (
+                      <div key={image.file_idx}>
+                        <div className="ImgCheckBox">
+                          <img
+                            className="MosaicImgs"
+                            src={
+                              "https://s3testsm.s3.amazonaws.com/" +
+                              image.file_name
+                            }
+                            alt=""
+                          />
+                          <input type="checkbox" id="ImgCheck" />
+                        </div>
+                      </div>
+                    ))}
+                  </div>
+
+                  {/* ///////////////////////////////////// */}
+                  {/* <div className="MosaicDate2">
+                  <div className="MosaicDates">2024-05-07</div>
+                  <div>
+                    <button className="MosaicFix">수정</button>
+                  </div>
+                </div> */}
+                  {/* <div className="MosaicImg">
+                  <div className="MosaicImgs">
+                    <div className="ImgCheck"></div>
+                  </div>
+                  <div className="MosaicImgs">
+                    <div className="ImgCheck"></div>
+                  </div>
+                  <div className="MosaicImgs">
+                    <div className="ImgCheck"></div>
+                  </div>
+                  <div className="MosaicImgs">
+                    <div className="ImgCheck"></div>
+                  </div>
+                  <div className="MosaicImgs">
+                    <div className="ImgCheck"></div>
+                  </div>
+                  <div className="MosaicImgs">
+                    <div className="ImgCheck"></div>
+                  </div>
+                  <div className="MosaicImgs">
+                    <div className="ImgCheck"></div>
+                  </div>
+                  <div className="MosaicImgs">
+                    <div className="ImgCheck"></div>
+                  </div>
+                  <div className="MosaicImgs">
+                    <div className="ImgCheck"></div>
+                  </div>
+                  <div className="MosaicImgs">
+                    <div className="ImgCheck"></div>
+                  </div>
+                  <div className="MosaicImgs">
+                    <div className="ImgCheck"></div>
+                  </div>
+                  <div className="MosaicImgs">
+                    <div className="ImgCheck"></div>
+                  </div>
+                  <div className="MosaicImgs">
+                    <div className="ImgCheck"></div>
+                  </div>
+                  <div className="MosaicImgs">
+                    <div className="ImgCheck"></div>
+                  </div>
+                  <div className="MosaicImgs">
+                    <div className="ImgCheck"></div>
+                  </div>
+                </div> */}
                 </div>
-              </div>
-              <div className="MosaicImg">
-                <div className="MosaicImgs">
-                  <div className="ImgCheck"></div>
-                </div>
-                <div className="MosaicImgs">
-                  <div className="ImgCheck"></div>
-                </div>
-                <div className="MosaicImgs">
-                  <div className="ImgCheck"></div>
-                </div>
-                <div className="MosaicImgs">
-                  <div className="ImgCheck"></div>
-                </div>
-                <div className="MosaicImgs">
-                  <div className="ImgCheck"></div>
-                </div>
-                <div className="MosaicImgs">
-                  <div className="ImgCheck"></div>
-                </div>
-                <div className="MosaicImgs">
-                  <div className="ImgCheck"></div>
-                </div>
-                <div className="MosaicImgs">
-                  <div className="ImgCheck"></div>
-                </div>
-                <div className="MosaicImgs">
-                  <div className="ImgCheck"></div>
-                </div>
-                <div className="MosaicImgs">
-                  <div className="ImgCheck"></div>
-                </div>
-                <div className="MosaicImgs">
-                  <div className="ImgCheck"></div>
-                </div>
-                <div className="MosaicImgs">
-                  <div className="ImgCheck"></div>
-                </div>
-                <div className="MosaicImgs">
-                  <div className="ImgCheck"></div>
-                </div>
-                <div className="MosaicImgs">
-                  <div className="ImgCheck"></div>
-                </div>
-                <div className="MosaicImgs">
-                  <div className="ImgCheck"></div>
-                </div>
-              </div>
+              ))}
             </div>
           </div>
         </div>
